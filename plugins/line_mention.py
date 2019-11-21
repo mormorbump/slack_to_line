aeeb8dac24996ee0be42c7c4513663eb57bec050
kazuki matsumoto <mormorbump@gmail.com>
2019-11-22 02:09:11 +0900

    aa

--- plugins/line_mention.py
+++ plugins/line_mention.py
@@ -4,17 +4,21 @@ import requests
 
 LINE_NOTIFY_ACCESS_TOKEN = os.environ["LINE_NOTIFY_ACCESS_TOKEN"]
 
-# メンションされた全てのメッセージ
-@respond_to('(.*)')
-def mention_func(message, something):
+# チャンネル内の全てのメッセージに応答。これをフックとしてmention_funcが走り、line_botへリクエストを送る。
+@listen_to('(.*)')
+def mention_func(message):
     # これでslackのユーザ名を取得可能。
-    name = "[" + message.channel._client.users[message._body['user']]['real_name'] + "@ slack]\n"
+    # name = "[" + message.channel._client.users[message._body['user']]['real_name'] + "@ slack]\n"
     # curlだったらこれ
     # -X httpメソッドの指定（今回はPOST）
     # -H "<header info>" ヘッダー情報の付与　\'Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}\'
     # -F "<key-value data>"フォームからファイルのアップロード。 データの指定(pythonの文字列処理っぽい) \'message={0}{1}\'".format(name,message.body['text'])
-    # "curl -X POST https://notify-api.line.me/api/notify -H \'Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}\' -F \'message={0}{1}\'".format(name,message.body['text'])
+    # "curl -X POST https://notify-api.line.me/api/notify -H 'Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}' -F 'message={0}{1}\'".format(name,message.body['text'])
     url = "https://notify-api.line.me/api/notify"
     headers = {"Authorization: Bearer %s" % LINE_NOTIFY_ACCESS_TOKEN}
-    payload = 'message=%s %s' % (name, message.body['text'])
-    r = requests.post(url, headers=headers, files=payload)
+    payload = 'message=%s' % (message.body['text'])
+    r = requests.post(url, headers=headers, files=dict(payload))
+
+@respond_to('(.*)')
+def mention_func(message):
+    message.reply('お前口臭くね？')
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,5 +2,7 @@ import os
 
 # 環境変数よりtokenを指定
 API_TOKEN = os.environ["API_TOKEN"]
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = ""
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ["plugins"]
