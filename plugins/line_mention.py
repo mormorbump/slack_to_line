a099fca8896892e8c38920817105262776d5f458
kazuki matsumoto <mormorbump@gmail.com>
2019-11-22 02:27:49 +0900

    aaa

--- plugins/line_mention.py
+++ plugins/line_mention.py
@@ -9,7 +9,7 @@ LINE_NOTIFY_ACCESS_TOKEN = os.environ["LINE_NOTIFY_ACCESS_TOKEN"]
 def listen_func(message, something):
     # これでslackのユーザ名を取得可能。
     # name = "[" + message.channel._client.users[message._body['user']]['real_name'] + "@ slack]\n"
-    
+
     # curlだったらこれ
     # -X httpメソッドの指定（今回はPOST）
     # -H "<header info>" ヘッダー情報の付与　\'Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}\'
@@ -17,7 +17,7 @@ def listen_func(message, something):
     # "curl -X POST https://notify-api.line.me/api/notify -H 'Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}' -F 'message={0}{1}\'".format(name,message.body['text'])
     url = "https://notify-api.line.me/api/notify"
     headers = {
-        "Authorization: Bearer %s" % LINE_NOTIFY_ACCESS_TOKEN
+        "Authorization": "Bearer %s" % LINE_NOTIFY_ACCESS_TOKEN
         }
     # payload = 'message=%s' % (message.body['text'])
     files = {
